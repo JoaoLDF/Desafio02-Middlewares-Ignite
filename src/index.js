a53c36f068a5ab28764b9d2b1223364c700fa68e
JoaoLDF <joaofilhodias3@gmail.com>
2021-07-16 18:33:10 -0300

    Desafio concluído

--- src/index.js
+++ src/index.js
@@ -10,19 +10,76 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({ message: "Not Found" });
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { pro, todos } = request.user;
+
+  if (todos.length < 10 && pro === false || pro === true) {
+    return next();
+  }
+
+  return response.status(403).json({ message: "Forbidden" });
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const uuidValidate = validate(id);
+
+  if (uuidValidate) {
+    const user = users.find(user => user.username === username);
+
+    if (!user) {
+
+      return response.status(404).json({ message: "Not Found" });
+
+    }
+
+    const todo = user.todos.find(todo => todo.id == id);
+
+    if (!todo) {
+
+      return response.status(404).json({ error: 404 });
+
+    }
+
+    request.user = user;
+    request.todo = todo;
+
+    return next();
+
+  }
+
+  return response.status(400).json({ message: "Bad Request" });
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ message: "Not Found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
